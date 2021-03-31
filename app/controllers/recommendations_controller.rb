f2bd1e3f868662c8624cf4db3701fec4a374017e
Elizabeth Parisi <parisielizabeth@gmail.com>
2021-03-31 19:38:49 -0400

    add index + show to recommendations controller

--- app/controllers/recommendations_controller.rb
+++ app/controllers/recommendations_controller.rb
@@ -1,2 +1,10 @@
 class RecommendationsController < ApplicationController
+
+  def index
+    @recommendations = Recommendation.all
+  end
+
+  def new
+    @recommendation = Recommendation.new
+  end
 end
