e37a0297a1b3a5959731b140d26dae1312378ea0
Elizabeth Parisi <parisielizabeth@gmail.com>
2021-03-31 17:58:03 -0400

    add current_user helper method to application controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,9 @@
 class ApplicationController < ActionController::Base
+
+  def current_user
+    # if @current_user isn't already defined it will find by id (and if there is a session id)
+    @current_user ||= User.find_by_id(session[:user_id]) if session[:user_id]
+  end
+
+
 end
