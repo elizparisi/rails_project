624ec01ed533ea14ee75f71fa67db301ffe718c5
Elizabeth Parisi <parisielizabeth@gmail.com>
2021-04-02 00:28:18 -0400

    add index and new to comments controller

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,2 +1,17 @@
 class CommentsController < ApplicationController
+
+  def index
+    @comments = Comment.all
+  end
+
+  def new
+    @comment = Comment.new
+  end
+
+  def create
+
+  end
+
+  def show
+  end
 end
