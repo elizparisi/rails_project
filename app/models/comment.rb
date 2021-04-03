efb49796e4e1660c926fc46c1b66bc11c200a361
Elizabeth Parisi <parisielizabeth@gmail.com>
2021-04-03 02:34:49 -0400

    add validations to comment model

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,4 +1,7 @@
 class Comment < ApplicationRecord
   belongs_to :user
   belongs_to :recommendation
+
+  validates :content, presence: true
+  validates :post, uniqueness: {scope: :user_id, message: "Only one comment please!"}
 end
