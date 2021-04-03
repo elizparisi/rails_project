644c495bee674f7b992f1cb4fcb80f53d46c8263
Elizabeth Parisi <parisielizabeth@gmail.com>
2021-04-03 02:29:02 -0400

    add validations to user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,5 +3,7 @@ class User < ApplicationRecord
   has_many :recommendations
   has_many :comments
   has_many :commented_recommendations, through: :comments, source: :recommendation
-  has_secure_password 
+  has_secure_password
+
+  validates :username, :email, presence: true 
 end
