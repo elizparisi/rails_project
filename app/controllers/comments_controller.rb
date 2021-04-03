cdb9ce02d84232cb878b77efedfa66670c07ab43
Elizabeth Parisi <parisielizabeth@gmail.com>
2021-04-02 21:43:21 -0400

    modify index in comments controller to reflect nesting

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,7 +1,14 @@
 class CommentsController < ApplicationController
 
   def index
-    @comments = Comment.all
+    #check to see if it's nested (is there a recommendation id?)
+    if params[:recommendation_id] && @recommendation = Recommendation.find_by_id(params[:recommendation_id])
+      #comments are nested
+      @comments = @recommendation.comments
+    else
+      #not nested 
+      @comments = Comment.all
+    end
   end
 
   def new
