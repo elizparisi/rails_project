a784359335c2dbe4388ee5c649af697d4403a913
Elizabeth Parisi <parisielizabeth@gmail.com>
2021-04-04 14:46:02 -0400

    add new comment form

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -22,10 +22,10 @@ class CommentsController < ApplicationController
   def create
     @comment = current_user.comments.build(comment_params)
 
-    if @comment.save 
+    if @comment.save
       redirect_to comments_path
     else
-      render: new
+      render :new
     end
   end
 
