cb1da0bc01320a4153859f0f378edb1e2c84992a
Elizabeth Parisi <parisielizabeth@gmail.com>
2021-04-04 16:18:06 -0400

    modify routes and users controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,28 +6,21 @@ class UsersController < ApplicationController
 
   def create
     @user = User.new(user_params)
+    # check to see if user saves
     if @user.save
-      session[:user_id] = @user.id
-      redirect_to user_path(@user)
+    # log them in and redirect to show page
+       session[:user_id] = @user.id
+       redirect_to user_path(@user)
     else
-      render :new
+       render :new
     end
-    # @user = User.new(user_params)
-    # # check to see if user saves
-    # if @user.save
-    #   # log them in and redirect to show page
-    #   session[:user_id] = @user.id
-    #   redirect_to user_path(@user)
-    # else
-    #   render :new
-    # end
   end
 
   def show
     redirect_if_not_logged_in
     @user = User.find_by_id(params[:id])
     #if it doesn't find id, this will give you nil instead of an error, redirect if no user is found
-    redirect_to root if !@user
+    redirect_to root_path if !@user
   end
 
   private
