4d832c7e39a143a7776645a0128a0014e7e79ca6
Elizabeth Parisi <parisielizabeth@gmail.com>
2021-04-05 01:16:49 -0400

    revise route order

--- config/routes.rb
+++ config/routes.rb
@@ -7,10 +7,11 @@ Rails.application.routes.draw do
   post '/login', to: 'sessions#create'
   delete '/logout', to: 'sessions#destroy'
 
+  resources :users
+  resources :comments
   resources :recommendations do
     resources :comments, only: [:index, :new, :create]
   end
 
-  resources :comments
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
