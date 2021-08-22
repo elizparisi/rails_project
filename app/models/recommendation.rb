95bd2cd656abb710932577f1cd8a5af2488a1c64
Elizabeth Parisi <parisielizabeth@gmail.com>
2021-08-22 16:41:51 -0400

    update validations with image

--- app/models/recommendation.rb
+++ app/models/recommendation.rb
@@ -3,12 +3,12 @@ class Recommendation < ApplicationRecord
   belongs_to :user
   has_many :comments, dependent: :destroy
   has_many :users, through: :comments
-  belongs_to :category
+  # belongs_to :category
 
   has_one_attached :image
 
 
-  validates :name, :image_url, :location, :description, presence: true
+  validates :name, :image, :location, :description, presence: true
 
   scope :alpha, -> { order (:name)}
 end
